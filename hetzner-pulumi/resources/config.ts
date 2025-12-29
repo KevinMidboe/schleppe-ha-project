cbb6c9034cacac9bff9e63bc00844ad202550971
Kevin Midboe <kevin.midboe@gmail.com>
2025-12-29 17:48:13 +0100

    removed variables from config

--- hetzner-pulumi/resources/config.ts
+++ hetzner-pulumi/resources/config.ts
@@ -2,15 +2,6 @@ import * as pulumi from "@pulumi/pulumi";
 
 const config = new pulumi.Config();
 
-const variables = {
-  osImage: config.get("image") || "debian-11",
-  machineType: config.get("serverType") || "f1-micro",
-  machineLocation:  config.get("location") || "hel1",
-  instanceTag:  config.get("instanceTag") || "webserver",
-  servicePort: config.get("servicePort") || "80" 
-}
-
 export {
-  variables,
   config
 }
